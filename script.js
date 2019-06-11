714d101c3c337011e872694a55eb19655a19efb6
Fábio Valentim <maitabom@gmail.com>
2019-06-11 08:44:47 -0300

    Implementação de filtros por estado

--- script.js
+++ script.js
@@ -14,4 +14,11 @@ $(document).ready(function () {
       $('.collapse.in').toggleClass('in');
       $('a[aria-expanded=true]').attr('aria-expanded', 'false');
   });
+
+  $("#buscar").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#estados li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
 });
