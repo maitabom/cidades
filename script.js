29005c5e9f9d5b467266eef2f3a544ebdbd44d3e
Fábio Valentim <maitabom@gmail.com>
2019-06-11 21:25:14 -0300

    Implementação da função de carga de dados

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
   $("#sidebar").mCustomScrollbar({
-      theme: "minimal"
+    theme: "minimal"
   });
 
   $('#dismiss, .overlay').on('click', function () {
-      $('#sidebar').removeClass('active');
-      $('.overlay').removeClass('active');
+    $('#sidebar').removeClass('active');
+    $('.overlay').removeClass('active');
   });
 
   $('#sidebarCollapse').on('click', function () {
-      $('#sidebar').addClass('active');
-      $('.overlay').addClass('active');
-      $('.collapse.in').toggleClass('in');
-      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    $('#sidebar').addClass('active');
+    $('.overlay').addClass('active');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
   });
 
-  $("#buscar").on("keyup", function() {
+  $("#buscar").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $("#estados li").filter(function() {
+    $("#estados li").filter(function () {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
@@ -26,20 +26,32 @@ $(document).ready(function () {
     type: "GET",
     url: "cidades.csv",
     dataType: "text",
-    success: function(data) {
+    success: function (data) {
       carregarDados(data);
     }
   });
 });
 
 function carregarDados(data) {
-    var lines = data.split(/\r?\n/);
-    var dados = [];
+  var lines = data.split(/\r?\n/);
+  var cidades = new ColecaoCidades();
 
-    for(var i = 1; i < lines.lenght; i++)
-    {
-        
-    }
-    
-    $('#preloader').fadeOut('slow');
+  for (var i = 1; i < lines.length && lines[i] != ""; i++) {
+    var line = lines[i];
+    var cols = line.split(',');
+    var estado = cols[0];
+    var municipio = cols[1];
+    var valor = cols[2];
+
+    cidade = new Cidade();
+    cidade.nome = municipio;
+    cidade.valor = valor;
+
+    cidades.adicionarCidade(estado, cidade);
+  }
+
+  cidades.sort();
+  cidades.save();
+
+  $('#preloader').fadeOut('slow');
 }
